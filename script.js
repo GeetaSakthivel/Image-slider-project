1857038eeb7b796922787e9458a79b6f058945a2
GeetaSakthivel <geetha28tech@gmail.com>
2024-07-29 22:11:48 +0800

    all image files are deleted

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
 
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slideWidth = slider.clientWidth;
+  slider.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex > 0) ? currentIndex - 1 : 0;
+  showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex < slider.children.length - 1) ? currentIndex + 1 : slider.children.length - 1;
+  showSlide(currentIndex);
+});
+
+window.addEventListener('resize', () => showSlide(currentIndex));
